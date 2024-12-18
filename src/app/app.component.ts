8df177fad0e027603e4accc08370e22104950b21
Rafael Cavalcante <rafaelcavalcante10.rc@gmail.com>
2024-12-18 17:09:12 -0300

    Criação de branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventosComponent } from "./eventos/eventos.component";
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EventosComponent, PalestrantesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
